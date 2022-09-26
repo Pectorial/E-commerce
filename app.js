601e69d138264f3c3a625bdbf16429314909eee1
princechibez <chibezprince@gmail.com>
2022-09-26 10:49:02 +0100

    still in setup mode

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv/config');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv/config");
+
+// Custom imports
+const connctDb = require("./Db/db");
 
 const app = express();
 
+app.use(express.json()) // For handling json payloads... i.e request bodies in json format
+app.use(express.urlencoded({ extended: false })) // For handling requests in x-www-urlencoded-form
 app.use(helmet());
-app.use(morgan());
+// app.use(morgan());
 
 // Configuring the server permissions for CORS protocols...
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
     methods: "POST, GET, PUT, PATCH, DELETE",
-    allowedHeaders: "Content-Type, Authorization"
-}))
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE")
-})
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, PATCH, DELETE"
+  );
+});
 
 // Dedicated middleware for handling errors/exceptions
 app.use((error, req, res, next) => {
-    res.status(error.statusCode || 500).json(error.message)
-})
+  res.status(error.statusCode || 500).json(error.message);
+});
 
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 5050;
 
-app.listen(port, () => console.log(`Connected to port ${port}`))
+connctDb(() => {
+  app.listen(port, () => console.log(`Connected to port ${port}`));
+});
