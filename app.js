a7b2104062fd6e7b3498fa06c440715e5fd597de
princechibez <chibezprince@gmail.com>
2022-09-26 10:13:22 +0100

    corrected the error middleware

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 })
 
 // Dedicated middleware for handling errors/exceptions
-app.use((req, res, next, error) => {
+app.use((error, req, res, next) => {
     res.status(error.statusCode || 500).json(error.message)
 })
 
